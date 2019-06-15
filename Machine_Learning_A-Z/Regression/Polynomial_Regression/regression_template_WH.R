771c32ab8ed5eec08d6888814d51911c78b7cc16
CrunchWillSupreme <33377681+CrunchWillSupreme@users.noreply.github.com>
2019-06-15 02:28:46 -0400

    Add files via upload

---
+++ Machine_Learning_A-Z/Regression/Polynomial_Regression/regression_template_WH.R
@@ -0,0 +1,27 @@
+# Regression Template
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting the Regression Model to the dataset
+# Create your regressor here
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualizing the Regression Model results (for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour  = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth of Bluff (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+
+                                      
